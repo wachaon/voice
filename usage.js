b2e7b7cafe8ac0876d7ba50b764a1ae6b32349d3
wachaon <wachaon@gmail.com>
2024-01-04 22:12:12 +0900

    利用するサンプルを増やす

--- usage.js
+++ usage.js
@@ -1,4 +1,4 @@
-const { speak, speach, getVoices, getVoiceID } = require('./index')
+const { speak, speach, getVoices, getVoiceID, getDict, setDict, deleteDict } = require('./index')
 
 speak('こんにちは 世界')
 speak('OK GOOGLE 今日の天気は', { Voice: getVoices().Microsoft.Haruka.Desktop })
@@ -10,3 +10,27 @@ speach(`私わたくしはその人を常に先生と呼んでいた。だから
 speach('九州そら。こんにちは 世界よ', { speaker: 17 })
 
 speach('私の名前は満別花丸。何やら外が騒がしいぞ！', { speaker: getVoiceID('満別花丸', 'ぶりっ子') })
+
+setDict({
+    surface: 'VOICEVOX',
+    pronunciation: "ボイスボックス",
+    accent_type: 5,
+    word_type: 'PROPER_NOUN',
+    priority: 5
+})
+
+speach('こちらにあるVOICEVOXを使用すれば簡単に音声を生成できます。', { speaker: getVoiceID('ずんだもん', 'ささやき') })
+
+const dict = getDict()
+Object.keys(dict).forEach(id => {
+    if (dict[id].surface === 'ＶＯＩＣＥＶＯＸ') {
+        console.log(() => ['ＶＯＩＣＥＶＯＸ', dict[id]])
+        console.log(() => deleteDict(id))
+    }
+    if (dict[id].surface === 'VOICEVOX') {
+        console.log(() => ['VOICEVOX', dict[id]])
+        console.log(() => deleteDict(id))
+    }
+})
+
+console.log(() => getDict())
