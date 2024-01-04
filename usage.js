602c1c080f766193143f28fc0579fd3bebdcd63b
wachaon <wachaon@gmail.com>
2024-01-04 23:46:41 +0900

    特定の状況でエラーになるのでコメントで退避させる

--- usage.js
+++ usage.js
@@ -2,7 +2,7 @@ const { speak, speach, getVoices, getVoiceID, getDict, setDict, deleteDict } = r
 
 speak('こんにちは 世界')
 speak('OK GOOGLE 今日の天気は', { Voice: getVoices().Microsoft.Haruka.Desktop })
-speak('僕はずんだもん。アレクサ、今何時？', { Voice: getVoices().VOICEVOX['ずんだもん']['ノーマル'] })
+// speak('僕はずんだもん。アレクサ、今何時？', { Voice: getVoices().VOICEVOX['ずんだもん']['ノーマル'] })
 
 speach(`私わたくしはその人を常に先生と呼んでいた。だからここでもただ先生と書くだけで本名は打ち明けない。`,
     { speaker: Math.floor(Math.random() * 74) }
